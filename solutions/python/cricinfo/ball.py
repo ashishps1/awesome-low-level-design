55cce28c97ebd06511a4ea0a0dd47651f9c72ab5
Prerna Jain <jprerna@users.noreply.github.com>
2025-05-13 17:20:28 +0530

    Update ball.py
    Added decorator : dataclass.
    By using this decorator, we dont need to explicitly define the getter setter methods, we can directly access the class properties.

--- solutions/python/cricinfo/ball.py
+++ solutions/python/cricinfo/ball.py
@@ -1,18 +1,10 @@
+from dataclasses import dataclass 
+
+@dataclass
 class Ball:
-    def __init__(self, ball_number, bowler, batsman, result):
-        self.ball_number = ball_number
-        self.bowler = bowler
-        self.batsman = batsman
-        self.result = result
+    ball_number: int
+    bowler: str
+    batsman: str
+    result: str
 
-    def get_ball_number(self):
-        return self.ball_number
-
-    def get_batsman(self):
-        return self.batsman
-
-    def get_bowler(self):
-        return self.bowler
-
-    def get_result(self):
-        return self.result
+    
