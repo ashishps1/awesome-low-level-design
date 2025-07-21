9d59468d8ca0e0d68475f83638b58bbe8df1ea4d
Somisetty <siva.d.somisetty@accenture.com>
2025-07-21 13:29:30 +0530

    Added [from account import Account] line in transaction.py

--- solutions/python/atm/transaction.py
+++ solutions/python/atm/transaction.py
@@ -1,7 +1,8 @@
 from abc import ABC, abstractmethod
+from account import Account
 
 class Transaction(ABC):
-    def __init__(self, transaction_id, account, amount):
+    def __init__(self, transaction_id, account: Account, amount):
         self.transaction_id = transaction_id
         self.account = account
         self.amount = amount
