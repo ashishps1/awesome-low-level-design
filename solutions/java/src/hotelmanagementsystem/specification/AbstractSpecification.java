6d3cbe2188449136e27f3f767e05505a5522ea81
ashx404 <ashishkhandelwal404@gmail.com>
2025-08-31 18:45:38 +0530

    refactor: cleaner specification implementation

--- solutions/java/src/hotelmanagementsystem/specification/AbstractSpecification.java
+++
@@ -1,7 +0,0 @@
-package hotelmanagementsystem.specification;
-
-public abstract class AbstractSpecification<T> implements Specification<T> {
-    public Specification<T> and(Specification<T> other) {
-        return new AndSpecification<>(this, other);
-    }
-}
