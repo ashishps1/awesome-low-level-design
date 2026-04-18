5f482fd287905f009b377e042775f5ba71a625a0
Sumit Raj <110073729+MilderBronze@users.noreply.github.com>
2026-04-18 15:41:22 +0530

    Refactor Vehicle class with primary constructor
    Refactored Vehicle class to use primary constructor.

--- solutions/csharp/ridesharingservice/Models/Vehicle.cs
+++ solutions/csharp/ridesharingservice/Models/Vehicle.cs
@@ -1,16 +1,5 @@
-class Vehicle
+class Vehicle(string licenseNumber, string model, RideType type)
 {
-    private readonly string licenseNumber;
-    private readonly string model;
-    private readonly RideType type;
-
-    public Vehicle(string license, string m, RideType t)
-    {
-        licenseNumber = license;
-        model = m;
-        type = t;
-    }
-
     public string LicenseNumber => licenseNumber;
     public string Model => model;
     public RideType Type => type;
